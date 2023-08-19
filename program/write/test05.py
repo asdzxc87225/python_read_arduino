0a731002095ed730a472ac9621e90a77f7703f1e
luu <asdzxc87225@gmail.com>
2023-08-19 16:24:21 +0800

    修改ui 界面

--- program/write/test05.py
+++ program/write/test05.py
@@ -2,7 +2,7 @@ import sys
 import serial
 import time
 import threading
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QComboBox, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QComboBox, QVBoxLayout, QWidget, QSpinBox, QLineEdit
 from datetime import datetime
 
 class RFTransmitterQtApp(QMainWindow):
@@ -26,26 +26,36 @@ class RFTransmitterQtApp(QMainWindow):
         self.baud_combobox.addItem("115200")
         # 添加更多波特率
 
-        self.mode_label = QLabel("發射模式：")
-        self.mode_combobox = QComboBox()
-        self.mode_combobox.addItem("每秒發射1到10")
-        self.mode_combobox.addItem("每100ms發射1到100")
-        self.mode_combobox.addItem("每10ms發射1到1000")
 
         self.transmit_button = QPushButton("開始發射")
         self.transmit_button.clicked.connect(self.start_transmission)
 
         self.status_label = QLabel("發射狀態：等待發射")
 
+        self.mode_data1 = QLabel("資料長度(byte):")
+        self.target1 = QLineEdit()
+        self.target1.setText('10')
+        self.mode_data2 = QLabel("資料總數(次數):")
+        self.target2 = QLineEdit()
+        self.target2.setText('5')
+        self.mode_data3 = QLabel("資料間隔時間(s):")
+        self.target3 = QLineEdit()
+        self.target3.setText('0.5')
+
         layout = QVBoxLayout()
         layout.addWidget(self.port_label)
         layout.addWidget(self.port_combobox)
         layout.addWidget(self.baud_label)
         layout.addWidget(self.baud_combobox)
-        layout.addWidget(self.mode_label)
-        layout.addWidget(self.mode_combobox)
-        layout.addWidget(self.transmit_button)
+        layout.addWidget(self.mode_data1)
+        layout.addWidget(self.target1)
+        layout.addWidget(self.mode_data2)
+        layout.addWidget(self.target2)
+        layout.addWidget(self.mode_data3)
+        layout.addWidget(self.target3)
+
         layout.addWidget(self.status_label)
+        layout.addWidget(self.transmit_button)
 
         central_widget = QWidget()
         central_widget.setLayout(layout)
